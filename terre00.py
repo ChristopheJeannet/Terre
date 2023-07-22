9760a596991226733bfdb477fa217f68614c030a
ChristopheJeannet <139656863+ChristopheJeannet@users.noreply.github.com>
2023-07-22 11:23:06 +0200

    Add files via upload

---
+++ terre00.py
@@ -0,0 +1,5 @@
+#py
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "k", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+for x in alphabet:
+    print(x)
