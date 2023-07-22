bd8375235434872599275dcf9114ec2cd2a9bc3d
ChristopheJeannet <139656863+ChristopheJeannet@users.noreply.github.com>
2023-07-22 16:21:00 +0200

    Add files via upload

---
+++ terre03.py
@@ -0,0 +1,7 @@
+#py
+
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+for x in alphabet:
+    if x < "n":
+        continue
+    print(x)
