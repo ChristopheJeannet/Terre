20129f1c13536157c61bac65429920000de45c9f
ChristopheJeannet <139656863+ChristopheJeannet@users.noreply.github.com>
2023-07-27 14:48:30 +0200

    Add files via upload
    S02 M03 - Épreuve de la Terre - Exercice " Afficheur d'arguments "

---
+++ terre02.py
@@ -0,0 +1,7 @@
+#py
+
+#S02 M03 - Épreuve de la Terre - Exercice " Afficheur d'arguments "
+
+les_arguments = ["je", "prends", "le", "temps", "chaque", "instant", "d'aller", "de","l'avant", "!"]
+for x in les_arguments:
+    print(x)
