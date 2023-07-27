e21013181d4b0e779c7ec0d305b1a98a58e3817e
ChristopheJeannet <139656863+ChristopheJeannet@users.noreply.github.com>
2023-07-27 14:44:56 +0200

    Update terre00.py
    S02 M03 - Épreuve de la Terre - Exercice " Alphabet "

--- terre00.py
+++ terre00.py
@@ -1,8 +1,6 @@
 #py
 
-# S02 M03 - Épreuve de la Terre
-
-# Exercice “Alphabet”
+# S02 M03 - Épreuve de la Terre - Exercice " Alphabet "
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "k", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 for x in alphabet:
