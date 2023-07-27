3b3b484f9fed88da571bf3230bf0c209b5e4a2be
ChristopheJeannet <139656863+ChristopheJeannet@users.noreply.github.com>
2023-07-27 14:40:57 +0200

    Update terre03.py
    S02 M03 - Épreuve de la Terre - Exercice " L"alphabet à partir de "

--- terre03.py
+++ terre03.py
@@ -1,5 +1,7 @@
 #py
 
+# S02 M03 - Épreuve de la Terre - Exercice " L"alphabet à partir de "
+
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 for x in alphabet:
     if x < "n":
